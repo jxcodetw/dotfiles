d2dba94725ba632c66913788bd125bbdf8347398
jxcodetw1 <jxcode.tw@gmail.com>
2018-02-02 15:06:57 +0800

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,14 @@
 if test "$(uname)" = "Darwin" ; then
   # MacOS
   install_cmd="brew install"
+  pip=""
 else
   # Linux
   install_cmd="sudo apt-get install"
+  pip="python3-pip"
 fi
 
-$install_cmd git zsh tmux python3 python3-pip
+$install_cmd git zsh tmux python3 $pip
 pip3 install npyscreen --user
 
 git clone https://github.com/jxcodetw/dotfiles.git ~/.dotfiles
